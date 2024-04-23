8718b72dce9e8feee39d6193091ae7436467cf94
IvanVokhmintsev <53772667+IvanVokhmintsev@users.noreply.github.com>
2024-04-23 17:44:16 +0300

    Добавлены комментарии к решению

--- copy.sh
+++ copy.sh
@@ -3,9 +3,9 @@ destRepo=$2
 
 
 IFS=$'\n'
-readarray -d '' files < <(find $srcRepo -mindepth 1  -maxdepth 1 -type f)
-readarray -d '' directories < <(find $srcRepo -mindepth 1 -maxdepth 1 -type d)
-readarray -d '' allFiles  < <(find $srcRepo -mindepth 1 -type f)
+readarray -d '' files < <(find $srcRepo -mindepth 1  -maxdepth 1 -type f) # получение списка всех файлов, находящихся непосредственно во входной директории
+readarray -d '' directories < <(find $srcRepo -mindepth 1 -maxdepth 1 -type d) # получение списка директорий, находящихся во входной директории
+readarray -d '' allFiles  < <(find $srcRepo -mindepth 1 -type f) # получение списка всех файлов, вложенных во входную директорию
 
 declare -A copiedFiles
 
@@ -13,6 +13,8 @@ for filePath in $allFiles
 do
     fileName=$(basename ${filePath})
     fileDirectory=$(dirname "$filePath")
+    # Реализована HashMap, подсчитывающая количество файлов по названию. При добавлении файла, значение инкрементируется.
+    # Если в HashMap уже есть файл с таким названием, то перед названием файла при копировании дописывается цифра в скобках
     if [ "${copiedFiles[$fileName]}" ]
     then
         counter=${copiedFiles[$fileName]}
